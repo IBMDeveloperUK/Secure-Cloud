19d522abe75b76599eae472b53a331d721c10660
Jenn Francis <jenn.francis@ibm.com>
2019-11-18 15:20:48 +0000

    Update setupVS.sh

--- setupVS.sh
+++ setupVS.sh
@@ -3,6 +3,8 @@
 apt update
 echo "Installing nano editor."
 yes Y | apt-get install nano
+echo "Installing curl."
+yes Y | apt-get install curl
 echo "Moving on to install git."
 apt install git
 echo "Cloning the directory for the tutorial."
