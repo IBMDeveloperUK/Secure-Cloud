e5bd1063aa0000efc0b4fb640f8df8ec49587d0e
Jenn Francis <jenn.francis@ibm.com>
2019-11-18 15:12:41 +0000

    Update setupVS.sh

--- setupVS.sh
+++ setupVS.sh
@@ -2,12 +2,12 @@
 
 apt update
 echo "Installing nano editor."
-apt-get install nano
+yes Y | apt-get install nano
 echo "Moving on to install git."
 apt install git
 echo "Cloning the directory for the tutorial."
-yes Y | git clone https://github.com/IBM/disaster-donations-website.git 
+yes Y | git clone https://github.com/IBMDeveloperUK/secure-cloud.git 
 echo "Installing Docker." 
 yes Y | apt-get install docker.io
 echo "Pre-reqs insttalled. Now it's time to get to work. Switching directory. . ." 
-cd /root/disaster-donations-website/backend
+cd $HOME/secure-cloud/backend
